cfe00960e5e74e55fbfa2d948abc3510919f1de3
eyouale <51370727+eyouale@users.noreply.github.com>
2021-05-29 10:05:08 +0300

    Add project files.

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,32 @@
+﻿using ec.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ec.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        private List<Post> _post;
+        public PostsController()
+        {
+            _post = new List<Post>();
+            for(int i = 0; i < 5; i++)
+            {
+                _post.Add(new Post { Id = Guid.NewGuid().ToString() }); 
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rng = "hisada";
+            return Ok(_post);
+        }
+    }
+}
