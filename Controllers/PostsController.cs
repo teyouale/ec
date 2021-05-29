92e6df58dd68ed047ab2226fbed9e9da6f2f382e
eyouale <51370727+eyouale@users.noreply.github.com>
2021-05-29 11:59:15 +0300

    configring Post Request

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -10,23 +10,34 @@ namespace ec.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PostsController : ControllerBase
+    public class PostsController : Controller
     {
         private List<Post> _post;
+        static List<string> strings = new List<string>()
+        {
+            "1","2","3"
+        };
+
+       
         public PostsController()
         {
             _post = new List<Post>();
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                _post.Add(new Post { Id = Guid.NewGuid().ToString() }); 
+                _post.Add(new Post { Id = Guid.NewGuid().ToString() });
             }
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IEnumerable<string> Get()
         {
-            var rng = "hisada";
-            return Ok(_post);
+            return strings;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            strings.Add(value);
         }
     }
 }
